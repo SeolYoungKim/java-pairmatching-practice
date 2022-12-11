5b2a93ccbeb640b9037781295cf6b6a24797cb25
SeolYoungKim <nasur4da@gmail.com>
2022-12-11 17:58:55 +0900

    refactor(InputView): 일부 출력 기능 OutputView로 위임

--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -2,12 +2,13 @@ package pairmatching.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.function.Supplier;
 import pairmatching.constants.Command;
 import pairmatching.dto.PairMatchingRequestDto;
 
 public class InputView {
-    public Command command() {
+    public Command selectFunction() {
         System.out.println(
                 "기능을 선택하세요.\n"
                 + "1. 페어 매칭\n"
@@ -21,17 +22,17 @@ public class InputView {
 
     public PairMatchingRequestDto pairMatching() {
         System.out.println(""
-                + "#############################################\n"
-                + "과정: 백엔드 | 프론트엔드\n"
-                + "미션:\n"
-                + "  - 레벨1: 자동차경주 | 로또 | 숫자야구게임\n"
-                + "  - 레벨2: 장바구니 | 결제 | 지하철노선도\n"
-                + "  - 레벨3: \n"
-                + "  - 레벨4: 성능개선 | 배포\n"
-                + "  - 레벨5: \n"
-                + "############################################");
+                + "과정, 레벨, 미션을 선택하세요.\n"
+                + "ex) 백엔드, 레벨1, 자동차경주");
 
         return untilInputIsCorrect(() -> new PairMatchingRequestDto(readLine().split(",")));
+    }
+
+    public String rematching() {
+        System.out.println(""
+                + "매칭 정보가 있습니다. 다시 매칭하시겠습니까?\n"
+                + "네 | 아니오");
+        return Console.readLine();
     }
 
     private <T> T untilInputIsCorrect(Supplier<T> supplier) {
