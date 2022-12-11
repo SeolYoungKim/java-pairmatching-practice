0adabb87c787317ba8ed146d7cd4a1570c1a456a
SeolYoungKim <nasur4da@gmail.com>
2022-12-11 17:59:26 +0900

    feat(Application): 통합 실행 기능 구현

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,23 @@
 package pairmatching;
 
+import pairmatching.controller.PairMachingMachine;
+import pairmatching.domain.CrewNameFileReader;
+import pairmatching.domain.CrewRepository;
+import pairmatching.domain.PairMatchResultRepository;
+import pairmatching.domain.PairMatcher;
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        PairMachingMachine pairMachingMachine = new PairMachingMachine(
+                new InputView(),
+                new OutputView(),
+                new PairMatcher(
+                        new CrewRepository(new CrewNameFileReader()),
+                        new PairMatchResultRepository())
+        );
+
+        pairMachingMachine.run();
     }
 }
